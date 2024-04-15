5835b779ee3e8e283512002e08a067691315c1fa
ac-scruffyk <acarva1@yahoo.com>
2024-04-15 16:54:35 -0500

    add crt-magnet-interference

--- js/script.js
+++ js/script.js
@@ -2,9 +2,37 @@ $(document).ready(function(){
   // projects view: reload the iframe and change its src attribute when a new project is selected
   let projectPreview = $('#projectPreview')[0];
   let projectSelect = $('#projectSelect');
+
+  // on changing the project selector, swap the iframe src and update the header/description fields
   projectSelect.change(function(e){
-    let projectUrl = './submods/' + this.value + '/markup.html'
-    projectPreview.setAttribute('src', projectUrl);
+    let projectName = this.value;
+    let project = projectData.projectList[projectName];
+    let projectMarkupUrl = './submods/' + projectName + '/markup.html'
+    projectPreview.setAttribute('src', projectMarkupUrl);
+
+    let projectElements = {
+      displayName: $('#projectName')[0],
+      descriptionText: $('#projectDescriptionText')[0],
+      gitHubUrl: $('#projectGitHubUrl a')[0],
+      tags: $('#projectTags')[0]
+    }
+
+    projectElements.displayName.innerText = project.displayName;
+    projectElements.descriptionText.innerHTML = convertMarkdownToHtml(project.descriptionText);
+    projectElements.gitHubUrl.innerText = project.gitHubUrl;
+    projectElements.gitHubUrl.href = project.gitHubUrl;
+  });
+
+  // random project selector button
+  let randomProjectButton = $('#randomProject');
+  randomProjectButton.click((e) => {
+    let projectNameArray = [];
+    for (let proj in projectData.projectList) {
+      if (proj != projectSelect[0].value) { projectNameArray.push(proj); }
+    }
+    let selection = projectNameArray[Math.floor(Math.random() * projectNameArray.length)];
+    projectSelect.val(selection);
+    projectSelect.trigger('change');
   });
 
   // global click operations
@@ -87,6 +115,14 @@ $(document).ready(function(){
   }
 });
 
+function convertMarkdownToHtml(markdownText) {
+  let text = markdownText.replace('<', '`');
+  text = text.replace('>', '`');
+  text = text.replace('`', '<pre><code>');
+  text = text.replace('`', '</code></pre>');
+  return text;
+}
+
 let projectData = {
   'categories': [
     'Games',
@@ -106,36 +142,55 @@ let projectData = {
     'Vanilla JavaScript',     // 8
     'JavaScript Libraries',   // 9
     'Gradients'               // 10
-
   ],
   'ctaList': [
     'Move the mouse cursor',
     'Click on the demo'
   ],
-  'projectList': [
-    {
+  'projectList': {
+    'dynamic-lighted-buttons': {
       'optValue': 'dynamic-lighted-buttons',
       'category': '1',
       'displayName': 'Dynamic Lighted Buttons',
-      'descriptionHtml': "In this proof of concept, the mouse cursor acts as a light source, causing the page background and several buttons on the page to illuminate and case shadows.\nThe page background effect is a simple radial gradient with a large radius (500px), centered on the mouse cursor.\nThe `fancy-button` class on the page buttons enables lighting effects for them. Similar to the page background, a radial gradient on each button simulates a light source emanating from the cursor. However, the gradient is significantly smaller, and its center is constrained to the button boundaries (the `left`, `right`, `top`, and `bottom` of the button's `BoundingClientRect`). The gradient also appears more diffuse when the mouse is off of and/or farther away from the button.\nThe shadows cast by the interaction of the buttons and [apparent] cursor light source are more diffuse when the mouse cursor is far from the button, and less diffuse when the cursor is closer to the button. The shadows also appear to move opposite the light source and grow slightly with more distance from the mouse cursor, as one would expect.\nThe behavior is triggered by the `mousemove` event, during which the position and diffusion of the gradients and shadows are calculated on a per-element basis.\nThis version improves on the calculations for `shadowXOffset`, `shadowYOffset`, `shadowDispersion`, and `shadowDepth`. It also reformats the setting of `proxy.style.boxShadow` to use all five possible `boxShadow` parameters, using the `shadowDispersion` value for both the blur radius and the spread radius of the shadow.",
-      'githubUrl': 'https://github.com/carvacodes/dynamic-lighted-buttons',
+      'descriptionText': "In this proof of concept, the mouse cursor acts as a light source, causing the page background and several buttons on the page to illuminate and case shadows.",
+      'gitHubUrl': 'https://github.com/carvacodes/dynamic-lighted-buttons',
       'tags': [8, 2]
     },
+    'canvas-composite-operations':
     {
       'optValue': 'canvas-composite-operations',
       'category': '1',
       'displayName': 'Canvas Composite Operations',
-      'descriptionHtml': "A quick look at the various composite operations provided to us by the `CanvasRenderingContext2D` interface.\nClick on the page to form a new curl, or check out the other animations using the **Animation** tab in the GUI. Check out https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation for the complete `CanvasRenderingContext2D` reference.\nThis demo also uses the [dat.GUI](https://github.com/dataarts/dat.gui) controller library for its graphical user interface.",
-      'githubUrl': 'https://github.com/carvacodes/canvas-composite-operations',
+      'descriptionText': "A quick look at the various composite operations provided to us by the `CanvasRenderingContext2D` interface.",
+      'gitHubUrl': 'https://github.com/carvacodes/canvas-composite-operations',
       'tags': [5, 8, 9, 10]
     },
+    'lissajous':
     {
       'optValue': 'lissajous',
       'category': '3',
       'displayName': 'Lissajous',
-      'descriptionHtml': "In this demo, an animated grid of lissajous curves are drawn to an HTML canvas using vanilla JavaScript.\nEach curve has a point (a \"dot\") associated with it that draws a piece of its curve on every window animation frame (via `window.requestAnimationFrame`). The farther right or farther down each grid square is, the faster its dot's X or Y coordinate (respectively) moves. Setting the HTML `<canvas>` element's `fadeIntensity` value to 0 initially allows the user to see the geometry of the curves as they relate to one another, and neatly visualizes how changing the X or Y oscillation speed changes the resulting graph.",
-      'githubUrl': 'https://github.com/carvacodes/lissajous',
+      'descriptionText': "In this demo, an animated grid of lissajous curves are drawn to an HTML `canvas` using vanilla JavaScript.",
+      'gitHubUrl': 'https://github.com/carvacodes/lissajous',
+      'tags': [5, 8, 10]
+    },
+    'moving-boxes':
+    {
+      'optValue': 'moving-boxes',
+      'category': '1',
+      'displayName': 'Moving Boxes Game',
+      'descriptionText': "A brief experiment with speeds, click handlers, etc. on the HTML `canvas`. Also a fun little game :)",
+      'gitHubUrl': 'https://github.com/carvacodes/moving-boxes',
+      'tags': [5, 8]
+    },
+    'crt-magnet-interference':
+    {
+      'optValue': 'crt-magnet-interference',
+      'category': '3',
+      'displayName': 'CRT Magnet Interference',
+      'descriptionText': "A cubic Bezier curve experiment that ended up simulating a kid, a magnet, and an old TV. Move the mouse, click to freeze.",
+      'gitHubUrl': 'https://github.com/carvacodes/crt-magnet-interference',
       'tags': [5, 8, 10]
     }
-  ]
+  }
 }
